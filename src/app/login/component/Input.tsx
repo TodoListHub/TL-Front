84bbb6cfd53002582506037c9cdfed611630adba
rza <rezaseydanloo.83@gmail.com>
2025-03-19 16:03:24 +0330

    username input completed

--- src/app/login/component/Input.tsx
+++ src/app/login/component/Input.tsx
@@ -1,19 +1,32 @@
+"use client"
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
+import useAuthStore from '../../../Store/useAuthStore';
 
-export default function Input({label} : {label:string}) {
+export default function Input() {
+
+  const {username , setUsername } = useAuthStore();
+  // too line bala az zustand tabe setusername ro farakhani kardam va ba estefade az
+  
+  //toye line paiin value ro az input username gereftam va dar store zakhire kardam 
+  const onClickhandler = (event: React.ChangeEvent<HTMLInputElement>)=> {
+    setUsername(event.target.value)
+  }
+
+
+
   return (
 
         <TextField
           className="w-[90%] h-12"
           id="standard-basic"
-          label={`${label}`}  
+          label="User Name"  
           variant="standard"
+          onChange={onClickhandler}
           sx={{ marginTop : 2 ,
-            "& label": { color: "teal" }, // تغییر رنگ لیبل
-            "& label.Mui-focused": { color: "teal" }, // تغییر رنگ لیبل در حالت فوکوس
-            "& .MuiInputBase-input": { color: "teal" }, // تغییر رنگ متن ورودی
-            
+            "& label": { color: "teal" },
+            "& label.Mui-focused": { color: "teal" }, 
+            "& .MuiInputBase-input": { color: "teal" }, 
           }}/>
 
     );
